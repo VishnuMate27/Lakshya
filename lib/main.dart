b7c6c0da8ae98e9c2971b4637e9aced0e9dd4257
VISHNU MATE <83972800+vishnumate@users.noreply.github.com>
2022-08-24 02:53:02 +0530

    Firebase database added

--- lib/main.dart
+++ lib/main.dart
@@ -9,10 +9,23 @@ import 'package:lakshya/Mobile%20Screeen%20Pages/registerScreen.dart';
 import 'package:lakshya/Mobile Screeen Pages/chooseStateScreen.dart';
 import 'package:lakshya/Mobile Screeen Pages/parentDashboardScreen.dart';
 import 'package:lakshya/Mobile Screeen Pages/studentDashboardScreen.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
+// Future<void> main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   await Firebase.initializeApp(options: DefaultFirebaseConfig.platformOptions);
+//
+//   if (shouldUseFirestoreEmulator) {
+//     FirebaseFirestore.instance.useFirestoreEmulator('localhost', 8080);
+//   }
+//   runApp(FirestoreExampleApp());
+// }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
